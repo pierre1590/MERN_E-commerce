ec15e895b1bc438f66210cd69460bdac5c10c7b4
Piero Sabino <piero.sa@icloud.com>
2022-02-08 22:47:40 +0100

    Add error 404

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,6 +1,17 @@
 import Icon from '../assets/sad-icon.jpg';
+import React, {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom'
 
 export default function NotFound () {
+const navigate = useNavigate();
+
+    // if page 404 redirect to home pages after 10 seconds
+    useEffect(() => {
+        setTimeout(() => {
+            navigate('/')
+        }, 10000)
+    }, [navigate])
+
   return (
     <> 
       <img src={Icon} alt="Not Found" style={{margin:'0 40%',width:'15%', height:'15%'}}/>
