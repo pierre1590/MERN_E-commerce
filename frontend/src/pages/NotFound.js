01ddd509ce77b1a7ec073e1fb3ec9d6d2dd0e9ef
Piero Sabino <piero.sa@icloud.com>
2022-02-08 22:40:38 +0100

    Update project

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -4,7 +4,7 @@ export default function NotFound () {
   return (
     <> 
       <img src={Icon} alt="Not Found" style={{margin:'0 40%',width:'15%', height:'15%'}}/>
-      <h1 className="error" style={{margin:'0 35%',color:'#f1a'}}>Ops, error 404 <br/>Page not found</h1>
+      <h1 className="error" style={{margin:'0 35%',color:'#f01'}}>Ops, error 404 <br/>Page not found</h1>
     </>
   )
 }
