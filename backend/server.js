fc032a07f828846927b99d6664636f2665db8af9
Piero Sabino <piero.sa@icloud.com>
2022-02-08 16:27:48 +0100

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,7 @@ if(process.env.NODE_ENV === 'development'){
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('API is running...' )
-})
+
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -37,10 +35,22 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
-
-
 const __dirname = path.resolve()
 app.use('/uploads',express.static(path.join(__dirname, '/uploads')))
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname,'/frontend/build')))
+
+  app.get('*', (req, res) => 
+      res.sendFile(path.resolve(__dirname, 'frontend','build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...' )
+})
+}
+
+
 
 app.use(notFound)
 
