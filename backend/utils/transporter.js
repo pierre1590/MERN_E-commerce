673e7f3763a28fcff0f18973f343cc7924e95481
Piero Sabino <piero.sa@icloud.com>
2022-02-09 12:33:16 +0100

    Update transporter.js

--- backend/utils/transporter.js
+++ backend/utils/transporter.js
@@ -4,6 +4,9 @@ dotenv.config();
 
 // configure the transporter for nodemailer to use gmail account to send mails
 const transporter = nodemailer.createTransport({
+	host:'smtp.gmail.com',
+	port: 465,
+	secure: true,
 	service: 'gmail',
 	auth: {
 		user: process.env.MAIL_USERNAME,
