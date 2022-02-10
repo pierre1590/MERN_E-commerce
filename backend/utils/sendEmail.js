0f34f3241d1e8ad601c140953b8186c303227640
Piero Sabino <piero.sa@icloud.com>
2022-02-10 12:01:54 +0100

    Update project

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -19,6 +19,9 @@ const sendEmail = async (id, email, option) => {
       to: email,
       title: "Activation Account",
       subject: "Confirm your email for E-SHOP", // Subject line
+	  headers: {
+		'X-Laziness-level': 1000,
+	  },
       html: `<div>
 			<h2>Account Created!</h2>
 			Click this link to 
