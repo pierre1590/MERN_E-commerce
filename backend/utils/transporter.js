a17331238b79e91142d9a2a8cd9ee4c8df6457d9
Piero Sabino <piero.sa@icloud.com>
2022-10-03 13:24:11 +0200

    Update project

--- backend/utils/transporter.js
+++ backend/utils/transporter.js
@@ -7,7 +7,7 @@ const transporter = nodemailer.createTransport({
 	service: 'gmail',
 	auth: {
 		user: process.env.MAIL_USERNAME,
-		pass: process.env.MAIL_PASSWORD,
+		pass: process.env.SECRET_PASSWORD,
 },
 tls: {
 	rejectUnauthorized: false
